de7b15c0df54c2b68c9c9da0b7db987a47f8d1d6
laptop <the.red.lemon@yandex.ru>
2023-04-30 21:53:25 +0300

    feat: create response for delete card

--- controllers/cards.js
+++ controllers/cards.js
@@ -9,10 +9,33 @@ module.exports.getCards = (req, res) => {
 
 module.exports.postCard = (req, res) => {
   const { name, link } = req.body;
-  console.log(req);
+  console.log(req.params);
+  console.log(req.body);
 
   cardSchema
     .create({ name, link, owner: req.user._id })
     .then((card) => res.send(card))
     .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.deleteCard = (req, res) => {
+  const { id } = req.params;
+
+  cardSchema
+    .findByIdAndRemove(id)
+    .orFail()
+    .then((card) => res.send(card))
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        res.status(404).send({ message: `Card Id: ${id} is not found` });
+        return;
+      }
+
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Invalid card id passed' });
+        return;
+      }
+
+      res.status(500).send({ message: err.message });
+    });
 };
--- routes/cards.js
+++ routes/cards.js
@@ -1,7 +1,8 @@
 const cardRouter = require('express').Router();
-const { getCards, postCard } = require('../controllers/cards');
+const { getCards, postCard, deleteCard } = require('../controllers/cards');
 
 cardRouter.get('/', getCards);
 cardRouter.post('/', postCard);
+cardRouter.delete('/:id', deleteCard);
 
 module.exports = cardRouter;
