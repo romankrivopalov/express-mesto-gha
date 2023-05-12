302c23f57f63bc16f3c88e815623bca1497ef7f2
laptop <the.red.lemon@yandex.ru>
2023-05-12 11:16:57 +0300

    refactor: change link for connect mongo

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(limiter);
 app.use(helmet());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {});
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
 
 app.use((req, res, next) => {
   req.user = {
