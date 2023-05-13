16da9bf5f80c66dad397e789f28425782c7d2889
laptop <the.red.lemon@yandex.ru>
2023-05-13 11:11:29 +0300

    feat: add middlewares auth

---
+++ middlewares/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+const handleAuthError = (res) => {
+  res
+    .status(401)
+    .send({ message: 'Необходима авторизация' });
+};
+
+const extractBearerToken = (header) => header.replace('Bearer ', '');
+
+module.exports.validateToken = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return handleAuthError(res);
+  }
+
+  const token = extractBearerToken(authorization);
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'secret-person-key');
+  } catch (err) {
+    return handleAuthError(res);
+  }
+
+  req.user = payload;
+
+  return next();
+};
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { validateToken } = require('../middlewares/auth');
 const { login, createUser } = require('../controllers/users');
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
@@ -6,8 +7,8 @@ const cardsRouter = require('./cards');
 router.post('/signin', login);
 router.post('/signup', createUser);
 
-router.use('/users', usersRouter);
-router.use('/cards', cardsRouter);
+router.use('/users', validateToken, usersRouter);
+router.use('/cards', validateToken, cardsRouter);
 router.use('/*', (req, res) => {
   res
     .status(404)
