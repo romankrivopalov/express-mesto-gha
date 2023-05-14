66e92fafd63b1b6ebebaaee6ea7d8e4b8c5d42d2
laptop <the.red.lemon@yandex.ru>
2023-05-14 17:11:20 +0300

    refactor: delete incommente line

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,7 @@
 const jwt = require('jsonwebtoken');
 const { UnathorizedError } = require('../utils/error');
 
-// const extractBearerToken = (header) => header.replace('Bearer ', '');
-
 module.exports.validateToken = (req, res, next) => {
-  // const { authorization } = req.headers;
-
-  // if (!authorization || !authorization.startsWith('Bearer ')) {
-  //   return next(new UnathorizedError('Authorization required'));
-  // }
-
   const token = req.cookies.jwt;
   let payload;
 
